3380e87f136d2c1d657eb5e7e0f0482c68998d76
Marcelo Junges <marcelo0363@hotmail.com>
2020-04-20 09:03:08 -0300

    mudança do head.

--- monitoramento1.0.sh
+++ monitoramento1.0.sh
@@ -2,54 +2,64 @@
 ##########################################################################
 #
 # MONITORAMENTO - CLIENTE 
-# Autor: Easytec Infomática - CNPJ 21.326.457/0001-60 - contato@easytecinformatica.com
+# Autor: Marcelo Junges - marcelo0363@hotmail.com
 # Funcao: Monitorar funcoes vitais do servidor
 # Data implatação: 28/07/2019
 # IP SERVIDOR: 0.0.0.0
 #
 # Instrucoes de uso:
 # implementar no crontab com o seguinte comando
-#00 05 * * 7 root /home/suporte/script/estado_servidor | mutt -s "Relatorio de monitoramento cliente" suporteasytec@gmail.com
+#00 21 * * * /scripts/monitoramento.sh | mutt -s "Relatorio de monitoramento cliente" seuemail@suaempresa.com
+#Com este comando o script rodará todo dia as 21h.
 #########################################################################
 #
 #
-echo "To:contato@easytecinformatica.com"
-echo "Subject:Relatorio de monitoramento Cliente"
+echo "MONITORAMENTO SERVIDOR EASYTEC INFORMATICA"
 #
 echo ""
 echo  "|--------------------------------------------------------------"
 echo ""
 echo "- Verificacao de rotina do estado do servidor"
-echo "Data: `date +%d/%m/%Y-%H:%M:%S`"
-echo "Máquina: `hostname`"
-echo "Tempo ligado: `uptime`"
+echo ""
+echo "* Data: `date +%d/%m/%Y-%H:%M:%S`"
+echo ""
+echo "* Máquina: `hostname`"
+echo ""
+echo "* Tempo ligado: `uptime | grep up`"
+echo ""
+echo " Ip Externo: `wget -qO- http://ipecho.net/plain`"
 echo ""
 #
 #Estado do HD
 echo "- Verificacao do estado dos HDs"
 echo ""
 #
-echo "- Espaco Ocupado"
-echo "`df -h`"
+echo "=> ESPAÇO OCUPADO"
+echo ""
+echo "`df -h | grep /dev/sd`"
 echo ""
 #
-echo "- HD SDA"
-echo "`smartctl -a /dev/sda | grep 'SMART overall-health self-assessment test result'`"
-echo "`smartctl -a /dev/sda | grep Reallocated_Sector_Ct`"
-echo "`smartctl -a /dev/sda | grep Current_Pending_Sector`"
-echo ""
 #
-#echo "- HD SDB"
+#OBS: descomentar as linhas de acordo a configuração do seu HD ou RAID no caso do mdadm.
+#echo "=> HD SDA"
+#echo ""
+#echo "`smartctl -a /dev/sda | grep 'SMART overall-health self-assessment test result'`"
+#echo "`smartctl -a /dev/sda | grep Reallocated_Sector_Ct`"
+#echo "`smartctl -a /dev/sda | grep Current_Pending_Sector`"
+#echo ""
+#
+#echo "=> HD SDB"
 #echo "`smartctl -a /dev/sdb | grep 'SMART overall-health self-assessment test result'`"
 #echo "`smartctl -a /dev/sdb | grep Reallocated_Sector_Ct`"
 #echo "`smartctl -a /dev/sdb | grep Current_Pending_Sector`"
 #echo ""
 #
-echo "- HD SDC"
-echo "`smartctl -a /dev/sdc | grep 'SMART overall-health self-assessment test result'`"
-echo "`smartctl -a /dev/sdc | grep Reallocated_Sector_Ct`"
-echo "`smartctl -a /dev/sdc | grep Current_Pending_Sector`"
-echo ""
+#echo "=> HD SDC"
+#echo ""
+#echo "`smartctl -a /dev/sdc | grep 'SMART overall-health self-assessment test result'`"
+#echo "`smartctl -a /dev/sdc | grep Reallocated_Sector_Ct`"
+#echo "`smartctl -a /dev/sdc | grep Current_Pending_Sector`"
+#echo ""
 #
 #Estado do RAID
 #echo "- MDADM /dev/md0"
@@ -76,18 +86,20 @@ echo ""
 #echo "`mdadm --detail /dev/md3 2>/dev/null | grep '/dev/s'`"
 #echo ""
 #
-#Temperatura
-echo "- Temperatura:"
-echo "`sensors | grep temp`"
+#SENSORES
+echo ""
+echo "=> SENSORES:"
+echo ""
+echo "`sensors`"
 echo ""
 #
 #RAM
-echo "- RAM:"
+echo "=> RAM:"
 echo "`free -m`"
 echo ""
 #
 #Backups
-echo "- Backups realizados"
+echo "=> BACKUPS REALIZADOS"
 echo "`cat /var/log/backup.log`"
 echo ""
 #
